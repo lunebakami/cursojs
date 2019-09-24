684251a4d08800b018de460d00fc04bc8fef5347
lunebakami <luis.santos.taiba@gmail.com>
2019-09-24 16:26:15 -0300

    att

---
+++ mains/mod2/functions.js
@@ -0,0 +1,14 @@
+export const idade = 23;
+
+
+export class Usuario {
+    static info() {
+        console.log('Apenas teste');
+    }
+}
+
+export default class User {
+    static info() {
+        console.log('Apenas teste');
+    }
+}
---
+++ mains/mod2/main.js
@@ -0,0 +1,8 @@
+import {Usuario as ClasseUsuario} from './functions';
+import {idade} from './functions';
+import User, {idade as idadeUsuario} from './functions';
+
+ClasseUsuario.info();
+User.info();
+console.log(idade);
+console.log(idadeUsuario);
